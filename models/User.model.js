91ede5adddd441eaa067f7c35dd0cf736ea1a623
Andrei Nazarov <andrei.nazarov@tum.de>
2024-01-25 11:28:56 +0100

    added auth

--- models/User.model.js
+++ models/User.model.js
@@ -1,16 +1,22 @@
-const { Schema, model } = require('mongoose')
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-    email: { type: String, required: true, trim: true, lowercase: true, unique: true },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
     hashedPassword: { type: String, required: true },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
-)
+);
 
-const User = model('User', userSchema)
+const User = model("User", userSchema);
 
-module.exports = User
+module.exports = User;
